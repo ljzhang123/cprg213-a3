f07c43241c8765a9706e7a40a4c4b91bf3925e7b
Jason Zhang <jason789.zhang@gmail.com>
2024-08-12 01:16:10 -0600

    finished js

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,6 +3,26 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
+let monButton = document.getElementById("monday");
+let tueButton = document.getElementById("tuesday");
+let wedButton = document.getElementById("wednesday");
+let thurButton = document.getElementById("thursday");
+let friButton = document.getElementById("friday");
+let clearButton = document.getElementById("clear-button");
+let fullButton = document.getElementById("full");
+let halfButton = document.getElementById("half");
+
+monButton.addEventListener("click", dayClicked);
+tueButton.addEventListener("click", dayClicked);
+wedButton.addEventListener("click", dayClicked);
+thurButton.addEventListener("click", dayClicked);
+friButton.addEventListener("click", dayClicked);
+clearButton.addEventListener("click", clearDays);
+fullButton.addEventListener("click", changeRate);
+halfButton.addEventListener("click", changeRate);
+
+let dailyRate = (document.getElementById("full").classList.contains("clicked")) ? 35:20;
+let dayCounter = 0;
 
 
 
@@ -10,14 +30,27 @@
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
-
-
-
+function dayClicked() {
+    if (!this.classList.contains("clicked")) {
+        this.classList.add("clicked");
+        dayCounter+=1;
+        calculate();
+    }
+}
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 
+function clearDays() {
+    let buttons = Array.from(document.getElementsByClassName("clicked"));
+    for (let button of buttons) {
+        button.classList.remove("clicked");
+    }
+    fullButton.classList.add("clicked");
 
+    let costDisplay = document.getElementById("calculated-cost");
+    costDisplay.innerHTML = "0";
+}
 
 
 
@@ -25,7 +58,20 @@
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
-
+function changeRate() {
+    if (this.innerHTML == "full" && !this.classList.contains("clicked")) {
+        dailyRate = 35;
+        this.classList.add("clicked");
+        halfButton.classList.remove("clicked");
+        calculate();
+    }
+    else if (this.innerHTML == "half" && !this.classList.contains("clicked")) {
+        dailyRate = 20;
+        this.classList.add("clicked");
+        fullButton.classList.remove("clicked");
+        calculate();
+    }
+}
 
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
@@ -37,4 +83,8 @@
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
-
+function calculate() {
+    let cost = dayCounter * dailyRate;
+    let costDisplay = document.getElementById("calculated-cost");
+    costDisplay.innerHTML = cost.toString();
+}
--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,12 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+let submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", onSubmit);
+
+
+function onSubmit() {
+    let page = document.getElementById("contact-page");
+    page.style.fontSize = "24px";
+    page.innerHTML = "<p>Thank you for your message<p>";
+}
